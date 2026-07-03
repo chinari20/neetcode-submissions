fe062ea7b87fa54dbf2c1ebd5b59b9af87f2653d
23wh1a0520 <150150962+chinari20@users.noreply.github.com>
2026-07-03 11:25:45 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> m = new HashMap<>();
+        for(String s:strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String k = new String(c);
+            m.putIfAbsent(k,new ArrayList<>());
+            m.get(k).add(s);
+        }
+        return new ArrayList<>(m.values());
+    }
+}
