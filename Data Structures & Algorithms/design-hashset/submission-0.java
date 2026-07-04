8bd1a630744bb42e045e23b98be4973eb1390ae7
23wh1a0520 <150150962+chinari20@users.noreply.github.com>
2026-07-04 14:29:25 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.java
@@ -0,0 +1,38 @@
+class MyHashSet {
+    HashSet<Integer> s;
+
+    public MyHashSet() {
+        s = new HashSet<>();
+        
+    }
+    
+    public void add(int key) {
+        s.add(key);
+        
+    }
+    
+    public void remove(int key) {
+        s.remove(key);
+        
+    }
+    
+    public boolean contains(int key) {
+        for(int i:s){
+            if(i==key){
+                return true;
+            }
+
+
+        }
+        return false;
+        
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
